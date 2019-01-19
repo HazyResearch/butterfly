fdd13c0c55eea3362c120f39b5cc97b0419bdba1
Tri Dao <tridpq@gmail.com>
2019-01-19 15:02:23 -0800

    Add Haar matrix as a target matrix

--- target_matrix.py
+++ target_matrix.py
@@ -9,6 +9,26 @@ import numpy as np
 import scipy.linalg as LA
 from scipy.fftpack import dct
 
+# Copied from https://stackoverflow.com/questions/23869694/create-nxn-haar-matrix
+def haar_matrix(n, normalized=False):
+    # Allow only size n of power 2
+    n = 2**np.ceil(np.log2(n))
+    if n > 2:
+        h = haar_matrix(n / 2)
+    else:
+        return np.array([[1, 1], [1, -1]])
+
+    # calculate upper haar part
+    h_n = np.kron(h, [1, 1])
+    # calculate lower haar part
+    if normalized:
+        h_i = np.sqrt(n/2)*np.kron(np.eye(len(h)), [1, -1])
+    else:
+        h_i = np.kron(np.eye(len(h)), [1, -1])
+    # combine parts
+    h = np.vstack((h_n, h_i))
+    return h
+
 
 def named_target_matrix(name, size):
     """
@@ -31,6 +51,8 @@ def named_target_matrix(name, size):
         np.random.seed(0)
         x = np.random.randn(size)
         return LA.circulant(x) / math.sqrt(size)
+    elif name == 'haar':
+        return haar_matrix(size, normalized=True) / math.sqrt(size)
     elif name == 'randn':
         np.random.seed(0)
         return np.random.randn(size, size) / math.sqrt(size)
