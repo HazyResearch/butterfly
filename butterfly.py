0b82ac2f16f4827b3cae13e7574d33a690a69492
Tri Dao <tridpq@gmail.com>
2019-03-06 19:40:09 -0800

    Implement weight-untying for butterfly

--- butterfly.py
+++ butterfly.py
@@ -276,20 +276,27 @@ class Block2x2DiagRectangular(nn.Module):
     subdiagonal and superdiagonal are nonzero.
     """
 
-    def __init__(self, size, stack=1, complex=False, ABCD=None):
+    def __init__(self, size, stack=1, complex=False, ABCD=None, n_blocks=1, tied_weight=True):
         """
         Parameters:
             size: input has shape (stack, ..., size)
             stack: number of stacked components, output has shape (stack, ..., size)
             complex: real or complex matrix
             ABCD: block of [[A, B], [C, D]], of shape (stack, 2, 2, size//2) if real or (stack, 2, 2, size//2, 2) if complex
+            n_blocks: number of such blocks of ABCD
+            tied_weight: whether the weights ABCD at different blocks are tied to be the same.
         """
         super().__init__()
         assert size % 2 == 0, 'size must be even'
         self.size = size
         self.stack = stack
         self.complex = complex
-        ABCD_shape = (stack, 2, 2, size // 2) if not complex else (stack, 2, 2, size // 2, 2)
+        self.n_blocks = n_blocks
+        self.tied_weight = tied_weight
+        if tied_weight:
+            ABCD_shape = (stack, 2, 2, size // 2) if not complex else (stack, 2, 2, size // 2, 2)
+        else:
+            ABCD_shape = (stack, n_blocks, 2, 2, size // 2) if not complex else (stack, n_blocks, 2, 2, size // 2, 2)
         scaling = 1.0 / 2 if complex else 1.0 / math.sqrt(2)
         if ABCD is None:
             self.ABCD = nn.Parameter(torch.randn(ABCD_shape) * scaling)
@@ -301,20 +308,28 @@ class Block2x2DiagRectangular(nn.Module):
         """
         Parameters:
             input: (stack, ..., size) if real or (stack, ..., size, 2) if complex
+            if not tied_weight: (stack, n_blocks, ..., size) if real or (stack, n_blocks, ..., size, 2) if complex
         Return:
             output: (stack, ..., size) if real or (stack, ..., size, 2) if complex
+            if not tied_weight: (stack, n_blocks, ..., size) if real or (stack, n_blocks, ..., size, 2) if complex
         """
-        if not self.complex:
-            return (self.ABCD.unsqueeze(1) * input.view(self.stack, -1, 1, 2, self.size // 2)).sum(dim=-2).view(input.shape)
+        if self.tied_weight:
+            if not self.complex:
+                return (self.ABCD.unsqueeze(1) * input.view(self.stack, -1, 1, 2, self.size // 2)).sum(dim=-2).view(input.shape)
+            else:
+                return complex_mul(self.ABCD.unsqueeze(1), input.view(self.stack, -1, 1, 2, self.size // 2, 2)).sum(dim=-3).view(input.shape)
         else:
-            return complex_mul(self.ABCD.unsqueeze(1), input.view(self.stack, -1, 1, 2, self.size // 2, 2)).sum(dim=-3).view(input.shape)
+            if not self.complex:
+                return (self.ABCD.unsqueeze(2) * input.view(self.stack, self.n_blocks, -1, 1, 2, self.size // 2)).sum(dim=-2).view(input.shape)
+            else:
+                return complex_mul(self.ABCD.unsqueeze(2), input.view(self.stack, self.n_blocks, -1, 1, 2, self.size // 2, 2)).sum(dim=-3).view(input.shape)
 
 
 class Block2x2DiagProductRectangular(nn.Module):
     """Product of block 2x2 diagonal matrices.
     """
 
-    def __init__(self, in_size, out_size, complex=False, decreasing_size=True, bias=True):
+    def __init__(self, in_size, out_size, complex=False, decreasing_size=True, tied_weight=True, bias=True):
         super().__init__()
         self.in_size = in_size
         m = int(math.ceil(math.log2(in_size)))
@@ -322,17 +337,26 @@ class Block2x2DiagProductRectangular(nn.Module):
         self.out_size = out_size
         self.stack = int(math.ceil(out_size / self.in_size_extended))
         self.complex = complex
+        self.tied_weight = tied_weight
         in_sizes = [self.in_size_extended >> i for i in range(m)] if decreasing_size else [self.in_size_extended >> i for i in range(m)[::-1]]
-        self.factors = nn.ModuleList([Block2x2DiagRectangular(in_size_, stack=self.stack, complex=complex) for in_size_ in in_sizes])
+        if tied_weight:
+            self.factors = nn.ModuleList([Block2x2DiagRectangular(in_size_, stack=self.stack, complex=complex)
+                                          for in_size_ in in_sizes])
+        else:
+            self.factors = nn.ModuleList([Block2x2DiagRectangular(in_size_, stack=self.stack, complex=complex, n_blocks=self.in_size_extended // in_size_, tied_weight=tied_weight)
+                                          for in_size_ in in_sizes])
         if bias:
-            self.bias = nn.Parameter(torch.Tensor(out_size))
+            if not self.complex:
+                self.bias = nn.Parameter(torch.Tensor(out_size))
+            else:
+                self.bias = nn.Parameter(torch.Tensor(out_size, 2))
         self.reset_parameters()
 
     def reset_parameters(self):
         """Initialize bias the same way as torch.nn.Linear."""
         if hasattr(self, 'bias'):
             bound = 1 / math.sqrt(self.in_size)
-            init.uniform_(self.bias, -bound, bound)
+            nn.init.uniform_(self.bias, -bound, bound)
 
     def forward(self, input):
         """
@@ -696,6 +720,20 @@ def test_block2x2diagproductrectangular():
     output = model(input)
     assert output.shape == (batch_size, out_size)
     model = Block2x2DiagProductRectangular(in_size, out_size, complex=True)
+    input = torch.randn((batch_size, in_size, 2))
+    output = model(input)
+    assert output.shape == (batch_size, out_size, 2)
+
+
+def test_block2x2diagproductrectangular_tied_weight():
+    batch_size = 3
+    in_size = 7
+    out_size = 15
+    model = Block2x2DiagProductRectangular(in_size, out_size, tied_weight=False)
+    input = torch.randn((batch_size, in_size))
+    output = model(input)
+    assert output.shape == (batch_size, out_size)
+    model = Block2x2DiagProductRectangular(in_size, out_size, complex=True, tied_weight=False)
     input = torch.randn((batch_size, in_size, 2))
     output = model(input)
     assert output.shape == (batch_size, out_size, 2)
