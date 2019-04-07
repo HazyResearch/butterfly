14410855b4c15e39fb8c5bb929b06d71d6c63c55
Tri Dao <tridpq@gmail.com>
2019-04-07 15:49:29 -0700

    Switch permutation multiply to decreasing stride order

--- tests/test_permutation_multiply.py
+++ tests/test_permutation_multiply.py
@@ -21,19 +21,20 @@ class PermutationMultTest(unittest.TestCase):
         m = int(math.log2(n))
         prob = torch.rand(m - 1, 3, requires_grad=True)
         for complex in [False, True]:
-            input = torch.randn((batch_size, n) + (() if not complex else (2, )), requires_grad=True)
-            output = permutation_mult(prob, input)
-            output_torch = permutation_mult_torch(prob, input)
-            self.assertTrue(torch.allclose(output, output_torch, rtol=self.rtol, atol=self.atol),
-                            (complex, (output - output_torch).abs().max().item()))
-            grad = torch.randn_like(output_torch)
-            d_prob, d_input = torch.autograd.grad(output, (prob, input), grad, retain_graph=True)
-            d_prob_torch, d_input_torch = torch.autograd.grad(output_torch, (prob, input), grad, retain_graph=True)
-            self.assertTrue(torch.allclose(d_input, d_input_torch, rtol=self.rtol, atol=self.atol),
-                            (complex, (d_input - d_input_torch).abs().max().item()))
-            # print((d_prob - d_prob_torch) / d_prob_torch)
-            self.assertTrue(torch.allclose(d_prob, d_prob_torch, rtol=self.rtol, atol=self.atol),
-                            (complex, ((d_prob - d_prob_torch) / d_prob_torch).abs().max().item()))
+            for increasing_stride in [False, True]:
+                input = torch.randn((batch_size, n) + (() if not complex else (2, )), requires_grad=True)
+                output = permutation_mult(prob, input)
+                output_torch = permutation_mult_torch(prob, input)
+                self.assertTrue(torch.allclose(output, output_torch, rtol=self.rtol, atol=self.atol),
+                                (complex, (output - output_torch).abs().max().item()))
+                grad = torch.randn_like(output_torch)
+                d_prob, d_input = torch.autograd.grad(output, (prob, input), grad, retain_graph=True)
+                d_prob_torch, d_input_torch = torch.autograd.grad(output_torch, (prob, input), grad, retain_graph=True)
+                self.assertTrue(torch.allclose(d_input, d_input_torch, rtol=self.rtol, atol=self.atol),
+                                (complex, (d_input - d_input_torch).abs().max().item()))
+                # print((d_prob - d_prob_torch) / d_prob_torch)
+                self.assertTrue(torch.allclose(d_prob, d_prob_torch, rtol=self.rtol, atol=self.atol),
+                                (complex, ((d_prob - d_prob_torch) / d_prob_torch).abs().max().item()))
 
     @unittest.skipIf(not torch.cuda.is_available(), "need CUDA")
     def test_permutation_cuda(self):
@@ -42,19 +43,20 @@ class PermutationMultTest(unittest.TestCase):
         m = int(math.log2(n))
         prob = torch.rand(m - 1, 3, device='cuda', requires_grad=True)
         for complex in [False, True]:
-            input = torch.randn((batch_size, n) + (() if not complex else (2, )), device=prob.device, requires_grad=True)
-            output = permutation_mult(prob, input)
-            output_torch = permutation_mult_torch(prob, input)
-            self.assertTrue(torch.allclose(output, output_torch, rtol=self.rtol, atol=self.atol),
-                            (complex, (output - output_torch).abs().max().item()))
-            grad = torch.randn_like(output_torch)
-            d_prob, d_input = torch.autograd.grad(output, (prob, input), grad, retain_graph=True)
-            d_prob_torch, d_input_torch = torch.autograd.grad(output_torch, (prob, input), grad, retain_graph=True)
-            self.assertTrue(torch.allclose(d_input, d_input_torch, rtol=self.rtol, atol=self.atol),
-                            (complex, (d_input - d_input_torch).abs().max().item()))
-            # print((d_prob - d_prob_torch) / d_prob_torch)
-            self.assertTrue(torch.allclose(d_prob, d_prob_torch, rtol=self.rtol, atol=self.atol),
-                            (complex, ((d_prob - d_prob_torch) / d_prob_torch).abs().max().item()))
+            for increasing_stride in [False, True]:
+                input = torch.randn((batch_size, n) + (() if not complex else (2, )), device=prob.device, requires_grad=True)
+                output = permutation_mult(prob, input)
+                output_torch = permutation_mult_torch(prob, input)
+                self.assertTrue(torch.allclose(output, output_torch, rtol=self.rtol, atol=self.atol),
+                                (complex, (output - output_torch).abs().max().item()))
+                grad = torch.randn_like(output_torch)
+                d_prob, d_input = torch.autograd.grad(output, (prob, input), grad, retain_graph=True)
+                d_prob_torch, d_input_torch = torch.autograd.grad(output_torch, (prob, input), grad, retain_graph=True)
+                self.assertTrue(torch.allclose(d_input, d_input_torch, rtol=self.rtol, atol=self.atol),
+                                (complex, (d_input - d_input_torch).abs().max().item()))
+                # print((d_prob - d_prob_torch) / d_prob_torch)
+                self.assertTrue(torch.allclose(d_prob, d_prob_torch, rtol=self.rtol, atol=self.atol),
+                                (complex, ((d_prob - d_prob_torch) / d_prob_torch).abs().max().item()))
 
 
 if __name__ == "__main__":
