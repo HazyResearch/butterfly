ba7612a6d2558661ef30f5021eb427794930b186
Albert Gu <albertgu@stanford.edu>
2019-05-04 14:40:36 -0700

    add dataset option with 2-sided permutation

--- cnn/dataset_utils.py
+++ cnn/dataset_utils.py
@@ -7,17 +7,48 @@ import torch
 import torchvision
 import torchvision.transforms as transforms
 
+import math
+import numpy as np
+def bitreversal_permutation(n):
+    """Return the bit reversal permutation used in FFT.
+    Parameter:
+        n: integer, must be a power of 2.
+    Return:
+        perm: bit reversal permutation, numpy array of size n
+    """
+    m = int(math.log2(n))
+    assert n == 1 << m, 'n must be a power of 2'
+    perm = np.arange(n).reshape(n, 1)
+    for i in range(m):
+        n1 = perm.shape[0] // 2
+        perm = np.hstack((perm[:n1], perm[n1:]))
+    return perm.squeeze(0)
+
+
+
 def get_dataset(config_dataset):
-    if config_dataset['name'] in ['CIFAR10', 'PCIFAR10']:
+    if config_dataset['name'] in ['CIFAR10', 'PCIFAR10', 'PPCIFAR10']:
         if config_dataset['name'] == 'PCIFAR10':
             # fix permutation
             rng_state = torch.get_rng_state()
             torch.manual_seed(0)
             true_perm = torch.randperm(1024)
             torch.set_rng_state(rng_state)
-            permutation_transform = [transforms.Lambda(lambda x: x.view(-1,1024)[:,true_perm].view(-1,32,32))]
+            permutation_transforms = [transforms.Lambda(lambda x: x.view(-1,1024)[:,true_perm].view(-1,32,32))]
+        elif config_dataset['name'] == 'PPCIFAR10':
+            # rng_state = torch.get_rng_state()
+            # torch.manual_seed(0)
+            # true_perm1 = torch.randperm(32)
+            # true_perm2 = torch.randperm(32)
+            true_perm1 = bitreversal_permutation(32)
+            true_perm2 = bitreversal_permutation(32)
+            # torch.set_rng_state(rng_state)
+            def fn(x):
+                # dumb hack because torch doesn't support multiple LongTensor indexing
+                return x.transpose(-1,-2)[...,true_perm2].transpose(-1,-2)[...,true_perm1]
+            permutation_transforms = [transforms.Lambda(fn)]
         else:
-            permutation_transform = []
+            permutation_transforms = []
 
         normalize = transforms.Normalize(
             mean=[0.49139765, 0.48215759, 0.44653141],
@@ -30,13 +61,13 @@ def get_dataset(config_dataset):
             transforms.ToTensor(),
             # transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
             normalize
-        ] + permutation_transform)
+        ] + permutation_transforms)
 
         transform_test = transforms.Compose([
             transforms.ToTensor(),
             # transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
             normalize
-        ] + permutation_transform)
+        ] + permutation_transforms)
 
         trainset = torchvision.datasets.CIFAR10(root=project_root+'/data', train=True, download=True, transform=transform_train)
         validset = torchvision.datasets.CIFAR10(root=project_root+'/data', train=True, download=False, transform=transform_test)
@@ -54,6 +85,11 @@ def get_dataset(config_dataset):
         if config_dataset['name'] == 'PCIFAR10':
             trainloader.true_permutation = true_perm
             testloader.true_permutation = true_perm
+        elif config_dataset['name'] == 'PPCIFAR10':
+            trainloader.true_permutation1 = true_perm1
+            trainloader.true_permutation2 = true_perm2
+            testloader.true_permutation1 = true_perm1
+            testloader.true_permutation2 = true_perm2
         return trainloader, testloader
     else:
         assert False, 'Dataset not implemented'
