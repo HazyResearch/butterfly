16e57034c664cc42bba8c93eebcd96be25cea847
Tri Dao <tridpq@gmail.com>
2019-05-12 04:10:34 -0700

    Experiment with ODO param in Transformer attention

--- transformer/dynamic_conv_compression.sh
+++ transformer/dynamic_conv_compression.sh
@@ -42,8 +42,26 @@
 # done
 # wait
 
-python dynamic_conv_experiment.py with ntrials=1 model=Transformer structure_type=B nblocks=0 n_encoder_structure_layer=6 n_decoder_structure_layer=6 structured_attention=True
-python dynamic_conv_experiment.py with ntrials=1 model=Transformer structure_type=B nblocks=1 n_encoder_structure_layer=6 n_decoder_structure_layer=6 structured_attention=True
-python dynamic_conv_experiment.py with ntrials=1 model=Transformer structure_type=B nblocks=2 n_encoder_structure_layer=6 n_decoder_structure_layer=6 structured_attention=True
-python dynamic_conv_experiment.py with ntrials=1 model=Transformer structure_type=B nblocks=4 n_encoder_structure_layer=6 n_decoder_structure_layer=6 structured_attention=True
-python dynamic_conv_experiment.py with ntrials=1 model=Transformer structure_type=B nblocks=6 n_encoder_structure_layer=6 n_decoder_structure_layer=6 structured_attention=True
+# For Google Cloud:
+# cd learning-circuits && git pull && cd butterfly/factor_multiply && python setup.py install && cd ../../fairseq && git pull && cd ../transformer
+# # p100-template-1
+# python dynamic_conv_experiment.py with ntrials=1 model=Transformer structure_type=B nblocks=0 n_encoder_structure_layer=6 n_decoder_structure_layer=6 structured_attention=True; gcloud compute instances stop $(hostname) --zone us-west1-b -q
+# # p100-template-2
+# python dynamic_conv_experiment.py with ntrials=1 model=Transformer structure_type=B nblocks=1 n_encoder_structure_layer=6 n_decoder_structure_layer=6 structured_attention=True; gcloud compute instances stop $(hostname) --zone us-west1-b -q
+# # p100-template-3
+# python dynamic_conv_experiment.py with ntrials=1 model=Transformer structure_type=B nblocks=2 n_encoder_structure_layer=6 n_decoder_structure_layer=6 structured_attention=True; gcloud compute instances stop $(hostname) --zone us-west1-b -q
+# # p100-template-4
+# python dynamic_conv_experiment.py with ntrials=1 model=Transformer structure_type=B nblocks=4 n_encoder_structure_layer=6 n_decoder_structure_layer=6 structured_attention=True; gcloud compute instances stop $(hostname) --zone us-west1-b -q
+# # p100-template-5
+# python dynamic_conv_experiment.py with ntrials=1 model=Transformer structure_type=B nblocks=6 n_encoder_structure_layer=6 n_decoder_structure_layer=6 structured_attention=True; gcloud compute instances stop $(hostname) --zone us-west1-b -q
+
+# k80-template-1
+python dynamic_conv_experiment.py with ntrials=1 model=Transformer structure_type=ODO nblocks=0 n_encoder_structure_layer=6 n_decoder_structure_layer=6 structured_attention=True; gcloud compute instances stop $(hostname) --zone us-west1-b -q
+# k80-template-2
+python dynamic_conv_experiment.py with ntrials=1 model=Transformer structure_type=ODO nblocks=1 n_encoder_structure_layer=6 n_decoder_structure_layer=6 structured_attention=True; gcloud compute instances stop $(hostname) --zone us-west1-b -q
+# p100-template-1
+python dynamic_conv_experiment.py with ntrials=1 model=Transformer structure_type=ODO nblocks=2 n_encoder_structure_layer=6 n_decoder_structure_layer=6 structured_attention=True; gcloud compute instances stop $(hostname) --zone us-west1-b -q
+# p100-template-2
+python dynamic_conv_experiment.py with ntrials=1 model=Transformer structure_type=ODO nblocks=4 n_encoder_structure_layer=6 n_decoder_structure_layer=6 structured_attention=True; gcloud compute instances stop $(hostname) --zone us-west1-b -q
+# p100-template-6
+python dynamic_conv_experiment.py with ntrials=1 model=Transformer structure_type=ODO nblocks=6 n_encoder_structure_layer=6 n_decoder_structure_layer=6 structured_attention=True; gcloud compute instances stop $(hostname) --zone us-west1-b -q
