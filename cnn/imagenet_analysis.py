b3d294f70f147031e7fe0544d0d20b666d2ad352
Tri Dao <tridpq@gmail.com>
2019-09-21 22:35:13 -0700

    Add numbers for butterfly and low rank MobileNet

--- cnn/imagenet_analysis.py
+++ cnn/imagenet_analysis.py
@@ -3,10 +3,18 @@ import json
 from pathlib import Path
 import numpy as np
 
-butterfly_acc = [56.4, 65.0, 71.1]
+# butterfly_acc = [56.4, 65.0, 70.1, 71.2]
+# butterfly_param = [0.70, 1.54, 3.62, 4.04]
+# Butterfly w/ channel pooling
+butterfly_smpool_acc = [54.0, 62.8, 68.4]
+butterfly_smpool_param = [0.44, 1.02, 2.60]
+# Butterfly w/ compressed softmax
+# butterfly_smstruct_acc = [59.0, 66.1, 70.5]
+# butterfly_smstruct_param = [0.60, 1.77, 5.69]
 width_acc = [51.6, 63.9, 70.9]
-butterfly_param = [0.7, 1.4, 4.0]
-width_param = [0.5, 1.4, 4.2]
+width_param = [0.47, 1.33, 4.23]
+lowrank_acc = [47.3, 56.1, 62.7]
+lowrank_param = [0.43, 1.00, 2.54]
 
 import matplotlib.pyplot as plt
 plt.switch_backend('agg')
@@ -14,8 +22,11 @@ plt.switch_backend('agg')
 markers = ['o', 'v', 'D', 'p', 's', '>']
 colors = ['red', 'orange', 'green', 'blue', 'black']
 
-plt.plot(butterfly_param, butterfly_acc, marker=markers[0], color=colors[0], label='Butterfly')
+# plt.plot(butterfly_param, butterfly_acc, marker=markers[0], color=colors[0], label='Butterfly')
 plt.plot(width_param, width_acc, marker=markers[2], color=colors[2], label='Reducing width')
+plt.plot(butterfly_smpool_param, butterfly_smpool_acc, marker=markers[0], color=colors[0], label='Kaleidoscope')
+plt.plot(lowrank_param, lowrank_acc, marker=markers[1], color=colors[1], label='Low-rank')
+# plt.plot(butterfly_smstruct_param, butterfly_smstruct_acc, marker=markers[3], color=colors[3], label='Butterfly w/ smstruct')
 # plt.plot(mobilenet_numparams / toeplitzlike_numparams[1:], all_accuracy[2, 1:], marker=markers[2], color=colors[2], label='Toeplitz-like')
 # plt.plot(mobilenet_numparams / resnet_numparams, resnet_accuracy, marker=markers[3], color=colors[3], label='Resnet18')
 # plt.plot(1, butterfly_acc, marker=markers[4], color=colors[4], label='MobileNet')
@@ -31,5 +42,6 @@ plt.ylabel('Accuracy (%)', fontsize=14)
 # plt.xticks([1, 2, 5, 10, 25], [1, 2, 5, 10, 25])
 
 # plt.legend(['Butterfly', 'Circulant', 'Toeplitz-like'])
-plt.legend(['Butterfly', 'Reducing width'])
+# plt.legend(['Butterfly', 'Reducing width'])
+plt.legend()
 plt.savefig('imagenet_compression.pdf', bbox_inches='tight')
