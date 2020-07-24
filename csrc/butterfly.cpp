87c8aeefff9fff6bb83ec3b8f512b4175833c7fc
Tri Dao <tridpq@gmail.com>
2020-07-24 13:30:44 -0700

    Implement bbs multiply fw cpu

--- csrc/butterfly.cpp
+++ csrc/butterfly.cpp
@@ -18,12 +18,10 @@ torch::Tensor butterfly_multiply_fw(const torch::Tensor& twiddle,
                                     const torch::Tensor& input,
                                     bool increasing_stride) {
   /* Parameters:
-         twiddle: (nstack, log n, n/2, 2, 2)
+         twiddle: (nstack, nblocks, log n, n/2, 2, 2)
          input: (batch_size, nstack, n)
          increasing_stride: whether to multiply with increasing stride (e.g. 1, 2, ..., n/2) or
              decreasing stride (e.g., n/2, n/4, ..., 1).
-             Note that this only changes the order of multiplication, not how twiddle is stored.
-             In other words, twiddle[@log_stride] always stores the twiddle for @stride.
      Returns:
          output: (batch_size, nstack, n)
   */
@@ -32,10 +30,11 @@ torch::Tensor butterfly_multiply_fw(const torch::Tensor& twiddle,
   const auto nstack = input.size(1);
   const auto n = input.size(2);
   const int log_n = int(log2((double) n));
-  TORCH_CHECK((twiddle.dim() == 5 && input.dim() == 3),
-              "butterfly_multiply_fw: twiddle and input must have dimension 5, 3");
-  TORCH_CHECK(twiddle.sizes() == torch::IntArrayRef({nstack, log_n, n / 2, 2, 2}),
-              "butterfly_multiply_fw: twiddle must have shape (nstack, log n, n/2, 2, 2)");
+  const int nblocks = twiddle.size(1);
+  TORCH_CHECK((twiddle.dim() == 6 && input.dim() == 3),
+              "butterfly_multiply_fw: twiddle and input must have dimension 6, 3");
+  TORCH_CHECK(twiddle.sizes() == torch::IntArrayRef({nstack, nblocks, log_n, n / 2, 2, 2}),
+              "butterfly_multiply_fw: twiddle must have shape (nstack, nblocks, log n, n/2, 2, 2)");
   if (input.device().is_cuda()) {
 #ifdef WITH_CUDA
     return butterfly_multiply_fw_cuda(twiddle, input, increasing_stride);
--- csrc/cpu/butterfly_cpu.cpp
+++ csrc/cpu/butterfly_cpu.cpp
@@ -7,27 +7,32 @@ torch::Tensor butterfly_multiply_fw_cpu(const torch::Tensor& twiddle,
   const auto nstack = input.size(1);
   const auto n = input.size(2);
   const int log_n = int(log2((double)n));
+  const int nblocks = twiddle.size(1);
   auto output =
-      torch::empty({batch_size, nstack, n},
-                   torch::dtype(input.dtype()).device(input.device()));
+    torch::empty({batch_size, nstack, n},
+                  torch::dtype(input.dtype()).device(input.device()));
   AT_DISPATCH_FLOATING_TYPES(input.scalar_type(), "butterfly_multiply_fw_cpu", [&] {
-    const auto twiddle_a = twiddle.accessor<scalar_t, 5>();
+    const auto twiddle_a = twiddle.accessor<scalar_t, 6>();
     auto input_a = input.accessor<scalar_t, 3>();
     auto output_a = output.accessor<scalar_t, 3>();
-    for (int64_t idx = 0; idx <= log_n - 1; ++idx) {
-      auto previous_a = idx == 0 ? input_a : output_a;
-      int64_t log_stride = increasing_stride ? idx : (log_n - 1 - idx);
-      int64_t stride = 1 << log_stride;
-      for (int64_t b = 0; b < batch_size; ++b) {
-        for (int64_t s = 0; s < nstack; ++s) {
-          for (int64_t i = 0; i < n / 2; ++i) {
-            int64_t low_order_bit = i % stride;
-            int64_t pos = 2 * (i - low_order_bit) + low_order_bit;
-            const scalar_t twiddle_val[2][2] = {{twiddle_a[s][log_stride][i][0][0], twiddle_a[s][log_stride][i][0][1]},
-                                                {twiddle_a[s][log_stride][i][1][0], twiddle_a[s][log_stride][i][1][1]}};
-            const scalar_t input_val[2] = {previous_a[b][s][pos], previous_a[b][s][pos + stride]};
-            output_a[b][s][pos] = twiddle_val[0][0] * input_val[0] + twiddle_val[0][1] * input_val[1];
-            output_a[b][s][pos + stride] = twiddle_val[1][0] * input_val[0] + twiddle_val[1][1] * input_val[1];
+    for (int64_t block = 0; block < nblocks; block++) {
+      bool cur_increasing_stride = increasing_stride != bool(block % 2);
+      for (int64_t idx = 0; idx <= log_n - 1; ++idx) {
+        auto previous_a = (idx == 0 && block == 0) ? input_a : output_a;
+        int64_t log_stride = cur_increasing_stride ? idx : (log_n - 1 - idx);
+        int64_t stride = 1 << log_stride;
+        for (int64_t b = 0; b < batch_size; ++b) {
+          for (int64_t s = 0; s < nstack; ++s) {
+            for (int64_t i = 0; i < n / 2; ++i) {
+              int64_t low_order_bit = i % stride;
+              int64_t pos = 2 * (i - low_order_bit) + low_order_bit;
+              const scalar_t twiddle_val[2][2] =
+                {{twiddle_a[s][block][idx][i][0][0], twiddle_a[s][block][idx][i][0][1]},
+                 {twiddle_a[s][block][idx][i][1][0], twiddle_a[s][block][idx][i][1][1]}};
+              const scalar_t input_val[2] = {previous_a[b][s][pos], previous_a[b][s][pos + stride]};
+              output_a[b][s][pos] = twiddle_val[0][0] * input_val[0] + twiddle_val[0][1] * input_val[1];
+              output_a[b][s][pos + stride] = twiddle_val[1][0] * input_val[0] + twiddle_val[1][1] * input_val[1];
+            }
           }
         }
       }
