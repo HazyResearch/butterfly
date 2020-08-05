Redefine torch.view_as_real/complex with correct gradient

tests/test_special.py
@@ -9,6 +9,7 @@ from torch import nn
 from torch.nn import functional as F
 
 import torch_butterfly
+from torch_butterfly.complex_utils import view_as_real, view_as_complex
 
 
 class ButterflySpecialTest(unittest.TestCase):
@@ -22,8 +23,8 @@ class ButterflySpecialTest(unittest.TestCase):
         n = 16
         input = torch.randn(batch_size, n, dtype=torch.complex64)
         for normalized in [False, True]:
-            out_torch = torch.view_as_complex(torch.fft(torch.view_as_real(input),
-                                                        signal_ndim=1, normalized=normalized))
+            out_torch = view_as_complex(torch.fft(view_as_real(input),
+                                                  signal_ndim=1, normalized=normalized))
             for br_first in [True, False]:
                 b = torch_butterfly.special.fft(n, normalized=normalized, br_first=br_first)
                 out = b(input)
@@ -34,8 +35,8 @@ class ButterflySpecialTest(unittest.TestCase):
         n = 16
         input = torch.randn(batch_size, n, dtype=torch.complex64)
         for normalized in [False, True]:
-            out_torch = torch.view_as_complex(torch.ifft(torch.view_as_real(input),
-                                                         signal_ndim=1, normalized=normalized))
+            out_torch = view_as_complex(torch.ifft(view_as_real(input),
+                                                   signal_ndim=1, normalized=normalized))
             for br_first in [True, False]:
                 b = torch_butterfly.special.ifft(n, normalized=normalized, br_first=br_first)
                 out = b(input)

tests/test_torch_butterfly.py
@@ -4,7 +4,9 @@ import unittest
 import numpy as np
 
 import torch
+
 import torch_butterfly
+from torch_butterfly.complex_utils import view_as_complex
 
 
 class ButterflyTest(unittest.TestCase):
@@ -29,7 +31,7 @@ class ButterflyTest(unittest.TestCase):
                                 self.assertTrue(output.shape == (batch_size, out_size),
                                                 (output.shape, device, (in_size, out_size), complex, ortho_init, nblocks))
                                 if ortho_init:
-                                    twiddle = b.twiddle if not b.complex else torch.view_as_complex(b.twiddle)
+                                    twiddle = b.twiddle if not b.complex else view_as_complex(b.twiddle)
                                     twiddle_np = twiddle.detach().to('cpu').numpy()
                                     twiddle_np = twiddle_np.reshape(-1, 2, 2)
                                     twiddle_norm = np.linalg.norm(twiddle_np, ord=2, axis=(1, 2))
@@ -43,8 +45,8 @@ class ButterflyTest(unittest.TestCase):
             log_n = int(math.log2(n))
             nstack = 2
             nblocks = 3
-            # for device in ['cpu'] + ([] if not torch.cuda.is_available() else ['cuda']):
-            for device in ['cuda']:
+            for device in ['cpu'] + ([] if not torch.cuda.is_available() else ['cuda']):
+            # for device in ['cuda']:
                 for complex in [False, True]:
                 # for complex in [False]:
                     for increasing_stride in [True, False]:
@@ -52,8 +54,8 @@ class ButterflyTest(unittest.TestCase):
                         if batch_size > 1024 and (device == 'cpu'):
                             continue
                         dtype = torch.float32 if not complex else torch.complex64
+                        # complex randn already has the correct scaling of stddev=1.0
                         scaling = 1 / math.sqrt(2)
-                        # scaling = 1 / math.sqrt(2) if not complex else 1 / 2  # TODO: complex randn already incorporate this scaling?
                         twiddle = torch.randn((nstack, nblocks, log_n, n // 2, 2, 2), dtype=dtype, requires_grad=True, device=device) * scaling
                         input = torch.randn((batch_size, nstack, n), dtype=dtype, requires_grad=True, device=twiddle.device)
                         output = torch_butterfly.butterfly_multiply(twiddle, input, increasing_stride)

torch_butterfly/butterfly.py
@@ -4,7 +4,8 @@ from torch import nn
 import torch.nn.functional as F
 
 from torch_butterfly.multiply import butterfly_multiply
-from torch_butterfly.complex_utils import real_dtype_to_complex
+from torch_butterfly.multiply import butterfly_multiply_torch
+from torch_butterfly.complex_utils import real_dtype_to_complex, view_as_real, view_as_complex
 
 
 class Butterfly(nn.Module):
@@ -41,7 +42,7 @@ class Butterfly(nn.Module):
         twiddle_core_shape = (self.nstacks, nblocks, log_n, size // 2)
         if not ortho_init:
             twiddle_shape = twiddle_core_shape + (2, 2)
-            # TODO: Check that complex randn has the correct variance
+            # complex randn already has the correct scaling of stddev=1.0
             scaling = 1.0 / math.sqrt(2)
             self.twiddle = nn.Parameter(torch.randn(twiddle_shape, dtype=dtype) * scaling)
         else:
@@ -73,9 +74,9 @@ class Butterfly(nn.Module):
         # This is used in optimizers such as SGD.
         # So we have to store the parameters as real.
         if complex:
-            self.twiddle = nn.Parameter(torch.view_as_real(self.twiddle))
+            self.twiddle = nn.Parameter(view_as_real(self.twiddle))
             if self.bias is not None:
-                self.bias = nn.Parameter(torch.view_as_real(self.bias))
+                self.bias = nn.Parameter(view_as_real(self.bias))
 
     def reset_parameters(self):
         """Initialize bias the same way as torch.nn.Linear."""
@@ -92,7 +93,7 @@ class Butterfly(nn.Module):
             output: (batch, *, out_size)
         """
         output = self.pre_process(input)
-        twiddle = self.twiddle if not self.complex else torch.view_as_complex(self.twiddle)
+        twiddle = self.twiddle if not self.complex else view_as_complex(self.twiddle)
         output = butterfly_multiply(twiddle, output, self.increasing_stride)
         return self.post_process(input, output)
 
@@ -112,7 +113,7 @@ class Butterfly(nn.Module):
         if self.out_size != out_size_extended:  # Take top rows
             output = output[:, :self.out_size]
         if self.bias is not None:
-            bias = self.bias if not self.complex else torch.view_as_complex(self.bias)
+            bias = self.bias if not self.complex else view_as_complex(self.bias)
             output = output + bias
         return output.view(*input.size()[:-1], self.out_size)
 

torch_butterfly/diagonal.py
@@ -5,6 +5,7 @@ import numpy as np
 import torch
 from torch import nn
 
+from torch_butterfly.complex_utils import view_as_real, view_as_complex
 from torch_butterfly.complex_utils import complex_mul, real_dtype_to_complex
 
 
@@ -28,7 +29,7 @@ class Diagonal(nn.Module):
             self.diagonal = nn.Parameter(torch.randn(size, dtype=dtype))
             self.complex = complex
         if self.complex:
-            self.diagonal = nn.Parameter(torch.view_as_real(self.diagonal))
+            self.diagonal = nn.Parameter(view_as_real(self.diagonal))
 
     def forward(self, input):
         """
@@ -37,5 +38,5 @@ class Diagonal(nn.Module):
         Return:
             output: (batch, *, size)
         """
-        diagonal = self.diagonal if not self.complex else torch.view_as_complex(self.diagonal)
+        diagonal = self.diagonal if not self.complex else view_as_complex(self.diagonal)
         return complex_mul(input, diagonal)

torch_butterfly/special.py
@@ -7,6 +7,7 @@ from torch.nn import functional as F
 from torch_butterfly.butterfly import Butterfly
 from torch_butterfly.permutation import FixedPermutation, bitreversal_permutation
 from torch_butterfly.diagonal import Diagonal
+from torch_butterfly.complex_utils import view_as_real, view_as_complex
 from torch_butterfly.complex_utils import complex_mul, real2complex, Real2Complex, Complex2Real
 
 
@@ -37,7 +38,7 @@ def fft(n, normalized=False, br_first=True, with_br_perm=True):
         twiddle /= math.sqrt(2)
     b = Butterfly(n, n, bias=False, complex=True, increasing_stride=br_first)
     with torch.no_grad():
-        torch.view_as_complex(b.twiddle).copy_(twiddle)
+        view_as_complex(b.twiddle).copy_(twiddle)
     if with_br_perm:
         br_perm = FixedPermutation(bitreversal_permutation(n, pytorch_format=True))
         return nn.Sequential(br_perm, b) if br_first else nn.Sequential(b, br_perm)
@@ -74,7 +75,7 @@ def ifft(n, normalized=False, br_first=True, with_br_perm=True):
         twiddle /= 2
     b = Butterfly(n, n, bias=False, complex=True, increasing_stride=br_first)
     with torch.no_grad():
-        torch.view_as_complex(b.twiddle).copy_(twiddle)
+        view_as_complex(b.twiddle).copy_(twiddle)
     if with_br_perm:
         br_perm = FixedPermutation(bitreversal_permutation(n, pytorch_format=True))
         return nn.Sequential(br_perm, b) if br_first else nn.Sequential(b, br_perm)
@@ -111,8 +112,7 @@ def circulant(col, transposed=False, separate_diagonal=True):
         col = real2complex(col)
     # This fft must have normalized=False for the correct scaling. These are the eigenvalues of the
     # circulant matrix.
-    col_f = torch.view_as_complex(torch.fft(torch.view_as_real(col),
-                                            signal_ndim=1, normalized=False))
+    col_f = view_as_complex(torch.fft(view_as_real(col), signal_ndim=1, normalized=False))
     if transposed:
         # We could have just transposed the iFFT * Diag * FFT to get FFT * Diag * iFFT.
         # Instead we use the fact that row is the reverse of col, but the 0-th element stays put.
@@ -130,7 +130,7 @@ def circulant(col, transposed=False, separate_diagonal=True):
     else:
         # Combine the diagonal with the last twiddle factor of b_fft
         with torch.no_grad():
-            twiddle = torch.view_as_complex(b_fft.twiddle)
+            twiddle = view_as_complex(b_fft.twiddle)
             twiddle[:, :, -1, :, 0, :] *= diag[::2].unsqueeze(-1)
             twiddle[:, :, -1, :, 1, :] *= diag[1::2].unsqueeze(-1)
         # Combine the b_fft and b_ifft into one Butterfly (with nblocks=2).
@@ -140,7 +140,7 @@ def circulant(col, transposed=False, separate_diagonal=True):
         b.in_size = n
         b.out_size = n
         with torch.no_grad():
-            # Don't need torch.view_as_complex here since all the twiddles are stored in real.
+            # Don't need view_as_complex here since all the twiddles are stored in real.
             b.twiddle.copy_(torch.cat((b_fft.twiddle, b_ifft.twiddle), dim=1))
         return b if complex else nn.Sequential(Real2Complex(), b, Complex2Real())
 
@@ -224,8 +224,7 @@ def conv1d_circular_multichannel(n, weight):
         col = real2complex(col)
     # This fft must have normalized=False for the correct scaling. These are the eigenvalues of the
     # circulant matrix.
-    col_f = torch.view_as_complex(torch.fft(torch.view_as_real(col),
-                                            signal_ndim=1, normalized=False))
+    col_f = view_as_complex(torch.fft(view_as_real(col), signal_ndim=1, normalized=False))
     br_perm = (bitreversal_permutation(n_extended, pytorch_format=True))
     col_f = col_f[..., br_perm]
     # We just want (input_f.unsqueeze(1) * col_f).sum(dim=2).
@@ -243,7 +242,7 @@ def conv1d_circular_multichannel(n, weight):
             self.diagonal = nn.Parameter(diagonal_init.detach().clone())
             self.complex = self.diagonal.is_complex()
             if self.complex:
-                self.diagonal = nn.Parameter(torch.view_as_real(self.diagonal))
+                self.diagonal = nn.Parameter(view_as_real(self.diagonal))
 
         def forward(self, input):
             """
@@ -252,7 +251,7 @@ def conv1d_circular_multichannel(n, weight):
             Return:
                 output: (batch, out_channels, size)
             """
-            diagonal = self.diagonal if not self.complex else torch.view_as_complex(self.diagonal)
+            diagonal = self.diagonal if not self.complex else view_as_complex(self.diagonal)
             return complex_mul(input.unsqueeze(1), diagonal).sum(dim=2)
 
     if not complex: